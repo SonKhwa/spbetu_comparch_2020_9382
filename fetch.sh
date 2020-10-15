04f25ae7010e19f17e61a0fdb2d9dfa6defc5113
JAkutenshi <jakutenshi@gmail.com>
2020-10-15 10:36:49 +0300

    Add new students to the scripts

--- fetch.sh
+++ fetch.sh
@@ -1,14 +1,14 @@
 git fetch S1ckick         # Субботин
 git fetch somewaytolive   # Круглова
 git fetch Valentina-Gol   # Голубева
-#git fetch                # 
-#git fetch                # 
-#git fetch                # 
-#git fetch                # 
-#git fetch                # 
-#git fetch                # 
-#git fetch                # 
-#git fetch                # 
+git fetch lastrise        # Русинов
+git fetch BabylonExile    # Михайлов
+git fetch dmitryduy       # Дерюгин
+git fetch VIkDema         # Демин
+git fetch Crunchy0        # Павлов
+git fetch alexundertable  # Кодуков
+git fetch Homry           # Сорокумов
+git fetch Indiora         # Савельев
 #git fetch                # 
 #git fetch                # 
 #git fetch                # 
--- remotes.sh
+++ remotes.sh
@@ -1,14 +1,14 @@
 git remote add S1ckick                   https://github.com/S1ckick/spbetu_comparch_2020_9382.git            # Субботин
 git remote add somewaytolive             https://github.com/somewaytolive/spbetu_comparch_2020_9382.git      # Круглова
 git remote add Valentina-Gol             https://github.com/Valentina-Gol/spbetu_comparch_2020_9382.git      # Голубева
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
-#git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
+git remote add lastrise                  https://github.com/lastrise/spbetu_comparch_2020_9382.git           # Русинов
+git remote add BabylonExile              https://github.com/BabylonExile/spbetu_comparch_2020_9382.git       # Михайлов
+git remote add dmitryduy                 https://github.com/dmitryduy/spbetu_comparch_2020_9382.git          # Дерюгин
+git remote add VIkDema                   https://github.com/VIkDema/spbetu_comparch_2020_9382.git            # Демин
+git remote add Crunchy0                  https://github.com/Crunchy0/spbetu_comparch_2020_9382.git           # Павлов
+git remote add alexundertable            https://github.com/alexundertable/spbetu_comparch_2020_9382.git     # Кодуков
+git remote add Homry                     https://github.com/Homry/spbetu_comparch_2020_9382.git              # Сорокумов
+git remote add Indiora                   https://github.com/Indiora/spbetu_comparch_2020_9382.git            # Савельев
 #git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
 #git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
 #git remote add                           https://github.com//spbetu_comparch_2020_9382.git                  # 
